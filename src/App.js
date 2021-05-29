ee95afe83003a18ee65c2dfa9634687edd839832
Moises Fuentes <moyxess@gmail.com>
2021-05-28 23:56:31 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './App.css';
+import Footer from './components/Footer'
+import Doge from './media/doge.png'
 
-function App() {
+const App = () => {
+
+  const [mem, setMem] = useState('');
+  const [doge, setDoge] = useState('');
+  const [cheem, setCheem] = useState('');
+
+
+  const handleChange = ({ target }) => {
+    setMem(target.value);
+  }
+
+  useEffect(() => {
+    if (mem) {
+      setDoge(mem);
+    }
+  }, [mem])
+
+
+
+  const projectCheem = () => {
+
+    let agresiv = 3;
+    let powerCheem = "m";
+    let countM = 0;
+    let stmtrminmg = [];
+    let string = doge.split("");
+    for (let i = 0; i < string.length; i++) {
+      countM++;
+      if (countM === agresiv) {               //Que tan agresivo quieres tu cheems: 1 super agresivo, +5 tu cheems no tan agresivo
+        countM = 0;
+        stmtrminmg.push(powerCheem);
+      }
+      stmtrminmg.push(string[i]);
+    }
+    stmtrminmg = stmtrminmg.join('');
+    setCheem(stmtrminmg);
+  }
+
+  const handleReset = () => {
+    setMem('');
+    setDoge('');
+    setCheem('');
+  }
+
+  const handleCopy = () => {
+    alert(`Tu CHEEM ha sido copiado`);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='bg'>
+        <div className='header'>
+          <div className='head-h1'>
+            <h1>Cheem converText</h1>
+          </div>
+          <div className='head-h3'>
+            <h3>Utiliza esta app con resonsabilidad</h3>
+          </div>
+        </div>
+        <div className='app'>
+          <input onChange={handleChange} input='text' value={mem} placeholder = 'Ingresa aquí el texto'/>
+          <div className='buttons'>
+            <button className='btn-cheem' onClick={projectCheem} >Cheems</button>
+            <button className='btn-clean' onClick={handleReset} >Limpiar</button>
+            <CopyToClipboard text={cheem} >
+              <button className='btn-copy' onClick={handleCopy}>Copiar texto</button>
+            </CopyToClipboard>
+          </div>
+          <div className='text-area'>
+            {cheem}
+          </div>
+        </div>
+        <img src={Doge} />
+      </div>
+      <footer>
+        <Footer />
+      </footer>
+
     </div>
+
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react'
+
+const Footer = () => {
+    return (
+        <Fragment>
+            <div class="contenedor-footer">
+                    <div class="content-foot">
+                        <h4>Celular</h4>
+                        <p>+52 4775676896</p>
+                    </div>
+                    <div class="content-foot">
+                        <h4>E-mail</h4>
+                        <p>moyxess@gmail.com</p>
+                    </div>
+                    <div class="content-foot">
+                        <h4>Localización</h4>
+                        <p>León. México</p>
+                    </div>
+                </div>
+                <h2 class="titulo-final">&copy; MoisesMC Design 👨‍💻Code&Code👨‍💻</h2>
+        </Fragment>
+    )
+}
+
+export default Footer
